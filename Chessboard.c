2297c4ad8fadb54c7133a3707f7110a5cfe7e1f7
DmitriiTelyakov <36217319+DmitriiTelyakov@users.noreply.github.com>
2018-02-18 23:06:16 +0700

    Update Chessboard.c

--- Chessboard.c
+++ Chessboard.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
-
-#define BLACK     "\033[1;30m"
+#define BLACK  "\033[1;30m"
 #define GREEN  "\033[1;32m"
-#define WHITE   "\033[1;37m"
+#define WHITE  "\033[1;37m"
 #define NONE 10
 
-enum {R,N,B,K,Q,P};
-enum {WH, BL};
+    enum {R,N,B,K,Q,P};
+    enum {WH, BL};
 
-typedef struct { 
-	char name:4 , col:2;
-}Cells;
+    typedef struct { 
+char name:4 , col:2;
+}	Cells;
 
 void drbo (Cells cell[] );
 void startpoz (Cells cell[] );
 
-
-
-
-
-
 int main ()
 {
-	Cells cell[63];
-	startpoz (cell);
-	drbo (cell);
-	scanf("a");
-	printf ("\n");
+    Cells cell[63];
+    startpoz (cell);
+    drbo (cell);
+    scanf("a");
+    printf ("\n");
 	return 0;
 }
 
-
 void startpoz (Cells cell[] ) {
-	int i = 0;
-	for (i = 0; i < 64 ; ++i) {
-		cell[i].name = NONE;
-		cell[i].col = 3;
-	}
+int i = 0;
+    for (i = 0; i < 64 ; ++i) {
+	cell[i].name = NONE;
+	cell[i].col = 3;
+}
 	cell[8].name = R;	
 		cell[8].col = BL;
 
@@ -60,7 +53,7 @@ void startpoz (Cells cell[] ) {
 
 	cell[64].name = R;	
 		cell[64].col = BL;
-	for (i = 7; i <= 7+(7*8); i+= 8) {
+    for (i = 7; i <= 7+(7*8); i+= 8) {
 		cell[i].name = P;
 		cell[i].col = BL;
 	}
@@ -87,99 +80,86 @@ void startpoz (Cells cell[] ) {
 
 	cell[57].name = R;	
 		cell[57].col = WH;
-	for (i = 2; i <= 2+(7*8); i+= 8) {
+    for (i = 2; i <= 2+(7*8); i+= 8) {
 		cell[i].name = P;
 		cell[i].col = WH;
-	}
-
-
 }
-
-
-
-
-
-
-
+}
 void drbo (Cells cell[] ) {
-	int i = 0, j = 0;
-	printf ("\n\n    _______________________________");
+int i = 0, j = 0;
+    printf ("\n\n    _______________________________");
 	
-	for (i = 8; i >= 1; --i) {
+    for (i = 8; i >= 1; --i) {
 		printf ("\n   | ");
-		for (j = i; j < i+(8*7)+1;j+= 8) {
-			switch (cell[j].col) {
-				case WH: 
-					printf ("%s",GREEN); 
-					break;
-				case BL: 
-					printf ("%s",BLACK); 
-					break;
-				default: 
-					break;
-			}
-			if (cell[j].col == WH) {
-				switch (cell[j].name) {
-				case R:
-					printf ("%c",'R');
-					break;
-				case N:
-					printf ("%c",'N');
-					break;
-				case B:
-					printf ("%c",'B');
-					break;
-				case Q:
-					printf ("%c",'Q');
-					break;
-				case K:
-					printf ("%c",'K');
-					break;
-				case P:
-					printf ("%c",'P');
-					break;
-				default:
-//					printf (" ");
-					break;
-				}
-			} else {if (cell[j].col == BL) {
-					switch (cell[j].name) {
-					case R:
-						printf ("%c",'r');
-						break;
-					case N:
-						printf ("%c",'n');
-						break;
-					case B:
-						printf ("%c",'b');
-						break;
-					case Q:
-						printf ("%c",'q');
-						break;
-					case K:
-						printf ("%c",'k');
-						break;
-					case P:
-						printf ("%c",'p');
-						break;
-					default:
-//						printf (" ");
-						break;
-					}
-				} else { 
-					printf (" ");
-				}
-			}
-			
-			if ( j != i+(8*7) ) {
-				printf ("\033[0m");
-				printf (" | ");
-			}
-		}
-		printf ("\033[0m |\n");
-		printf (" %d |___|___|___|___|___|___|___|___| ",i);
-	}
+    for (j = i; j < i+(8*7)+1;j+= 8) {
+	switch (cell[j].col) {
+	case WH: 
+	printf ("%s",GREEN); 
+	break;
+        case BL: 
+	printf ("%s",BLACK); 
+	break;
+	default: 
+	break;
+}
+	if (cell[j].col == WH) {
+	switch (cell[j].name) {
+	case R:
+	printf ("%c",'R');
+	break;
+	case N:
+        printf ("%c",'N');
+	break;
+	case B:
+	printf ("%c",'B');
+	break;
+	case Q:
+	printf ("%c",'Q');
+	break;
+	case K:
+	printf ("%c",'K');
+	break;
+	case P:
+	printf ("%c",'P');
+	break;
+	default:
+	break;
+}
+} 	else {if (cell[j].col == BL) {
+	switch (cell[j].name) {
+	case R:
+	printf ("%c",'r');
+	break;
+	case N:
+	printf ("%c",'n');
+	break;
+	case B:
+	printf ("%c",'b');
+	break;
+	case Q:
+	printf ("%c",'q');
+	break;
+	case K:
+	printf ("%c",'k');
+	break;
+	case P:
+	printf ("%c",'p');
+	break;
+	default:
+	break;
+}
+}       else { 
+	printf (" ");
+}
+}
+    if ( j != i+(8*7) ) {
+	printf ("\033[0m");
+	printf (" | ");
+}
+}
+	printf ("\033[0m |\n");
+	printf (" %d |___|___|___|___|___|___|___|___| ",i);
+}
 	printf ("\n     A   B   C   D   E   F   G   H\n");
-	
 }
 	
